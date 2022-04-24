dba66c42628446e340f32be396ab1a43961e9aa2
jamyyu <jamy11212@gmail.com>
2022-04-24 22:36:44 +0800

    change to while i > 0

--- password-retry.py
+++ password-retry.py
@@ -1,7 +1,7 @@
 #密碼重設程式
 password = 'a123456' #定義在迴圈外面
 x = 3 #定義在迴圈外面,剩餘機會
-while True:
+while x > 0:
     pwd = input('請輸入密碼：')
     if pwd == password:
         print('登入成功')
@@ -9,8 +9,4 @@ while True:
     else:
         x = x - 1
         print('密碼錯誤！還有', x, '次機會')
-        if x == 0:
-            print('密碼已鎖定')
-            break
-            
         
